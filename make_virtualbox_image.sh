1e4ee7024cd43b44b652294c48489ae49e02e09f
David Morhovich <pair+dmorhovich@pivotallabs.com>
2015-06-19 12:57:08 -0400

    wip

--- make_virtualbox_image.sh
+++ make_virtualbox_image.sh
@@ -21,6 +21,20 @@ then
   exit -1
 fi
 
+if [[ ! -f iso/windows/en_windows_server_2012_r2_x64_dvd_2707946.iso ]];
+then
+  echo "Windows 2012 ISO doesn't exist in iso/windows. Please copy it in there if you haven't done so. Thank you!"
+  exit -1
+fi
+
+
+echo "Verifying Windows 2012 ISO..."
+if [[ ! "$(openssl sha1 iso/windows/en_windows_server_2012_r2_x64_dvd_2707946.iso  | awk '{print $2}')" = "b6f063436056510357cb19cb77db781ed9c11df3" ]]
+then
+  echo "Unexpected sha1 for Windows 2012 ISO. Check that you put it in iso/other and that it is the correct version. Thanks!"
+  exit -1
+fi
+
 if [[ ! -f iso/other/en_visual_studio_ultimate_2013_with_update_4_x86_dvd_5935075.iso ]];
 then
   echo "Visual Studio ISO doesn't exist in iso/other. Please copy it in there if you haven't done so. Thank you!"
@@ -29,9 +43,9 @@ fi
 
 
 echo "Verifying Visual Studio ISO..."
-if [[ ! "$(openssl sha iso/other/en_visual_studio_ultimate_2013_with_update_4_x86_dvd_5935075.iso  | awk '{print $2}')" = "62c2f1500924e7b1402b6fcb9350ae9e0af444f9" ]]
+if [[ ! "$(openssl sha1 iso/other/en_visual_studio_ultimate_2013_with_update_4_x86_dvd_5935075.iso  | awk '{print $2}')" = "62c2f1500924e7b1402b6fcb9350ae9e0af444f9" ]]
 then
-  echo "Unexpected sha for Visual Studio ISO. Check that you put it in iso/other and that it is the correct version. Thanks!"
+  echo "Unexpected sha1 for Visual Studio ISO. Check that you put it in iso/other and that it is the correct version. Thanks!"
   exit -1
 fi
 
@@ -43,9 +57,9 @@ echo "Downloading Resharper..."
 [ -f iso/other/ReSharper.exe ] || curl -L -o "iso/other/ReSharper.exe" -C - "https://download.jetbrains.com/resharper/JetBrains.ReSharperUltimate.2015.1.1.exe"
 
 echo "Verifying Resharper..."
-if [[ ! "$(openssl sha iso/other/ReSharper.exe | awk '{print $2}')" = "dbf27641178ebee455b3aaacc6c001e522ebfda7" ]]
+if [[ ! "$(openssl sha1 iso/other/ReSharper.exe | awk '{print $2}')" = "e67cad599af5479072d052b59086ded854eb404e" ]]
 then
-  echo "Unexpected sha for Resharper. Check that it correctly downloaded and the link in this script isn't stale."
+  echo "Unexpected sha1 for Resharper. Check that it correctly downloaded and the link in this script isn't stale."
   exit -1
 fi
 
