f458f7a9d6104e93358700d7136c720ebb4733a3
pivotal <pivotal@warren.ny.pivotallabs.com>
2015-07-29 16:21:45 -0400

    wip

--- make_virtualbox_image.sh
+++ make_virtualbox_image.sh
@@ -15,30 +15,12 @@ then
   exit -1
 fi
 
-if [[ ! "$(packer version | sed -n '1p')" = "Packer v0.7.5" ]]
+if [[ ! "$(packer version | sed -n '1p')" = "Packer v0.8.2" ]]
 then
-  echo "Only known to work with packer version 0.7.5"
+  echo "Only known to work with packer version 0.8.2"
   exit -1
 fi
 
-
-if [[ ! -f iso/windows/en_windows_server_2012_r2_with_update_x64_dvd_6052708.iso ]];
-then
-  echo "Windows 2012 ISO doesn't exist in iso/windows. Please copy it in there if you haven't done so. Thank you!"
-  echo "Try https://msdn.microsoft.com/subscriptions/json/GetDownloadRequest?brand=MSDN&locale=en-us&fileId=62611&activexDisabled=true&akamaiDL=false"
-  echo "Thanks, bye!"
-  exit -1
-fi
-
-
-echo "Verifying Windows 2012 ISO..."
-if [[ ! "$(openssl sha1 iso/windows/en_windows_server_2012_r2_with_update_x64_dvd_6052708.iso  | awk '{print $2}')" = "865494e969704be1c4496d8614314361d025775e" ]]
-then
-  echo "Unexpected sha1 for Windows 2012 ISO. Check that you put it in iso/windows and that it is the correct version."
-  echo "Try https://msdn.microsoft.com/subscriptions/json/GetDownloadRequest?brand=MSDN&locale=en-us&fileId=62611&activexDisabled=true&akamaiDL=false"
-  echo "Thanks!"
-  exit -1
-fi
 
 if [[ ! -f iso/en_visual_studio_ultimate_2013_with_update_4_x86_dvd_5935075.iso ]];
 then
@@ -74,5 +56,3 @@ fi
 
 echo "Splicing in key..."
 sed -i.bak -e 's/WIN2012_KEY/'$WIN2012_KEY'/' answer_files/2012_r2/Autounattend.xml
-echo "Starting packer..."
-packer build --only=virtualbox-iso windows_2012_r2.json
