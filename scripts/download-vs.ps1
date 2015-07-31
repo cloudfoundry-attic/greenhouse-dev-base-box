039e63ada4d009961b4e5463ccf43ae2627f6296
pivotal <pivotal@warren.ny.pivotallabs.com>
2015-07-31 15:32:27 -0400

    WIP

---
+++ scripts/download-vs.ps1
@@ -0,0 +1,6 @@
+cd
+git clone https://github.com/mavenraven/msdn-downloader
+cd msdn-downloader
+ $env:GOPATH = "$PWD\Godeps\_workspace"
+
+
--- scripts/misc-packages.bat
+++ scripts/misc-packages.bat
@@ -3,3 +3,5 @@ choco install -y git.install -params /GitOnlyOnPath
 choco install -y fiddler4
 choco install -y sysinternals
 choco install -y winscp
+choco install -y phantomjs -version 2.0.0 
+choco install -y golang -version 1.4.2 
